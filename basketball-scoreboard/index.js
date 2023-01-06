f7f9a2a903fdb280aa086223197775704f5834fc
juditkisistok <judit.kisistok@gmail.com>
2023-01-06 19:30:55 +0100

    making all the buttons work

--- basketball-scoreboard/index.js
+++ basketball-scoreboard/index.js
@@ -1,21 +1,32 @@
 let homeScore = 0;
 let guestScore = 0;
 
-let homeEl = document.querySelector(".home .score")
-let guestEl = document.querySelector(".guest .score")
+const homeEl = document.querySelector(".home .score")
+const guestEl = document.querySelector(".guest .score")
 
-let homeOne = document.querySelector(".home .increment-btn .one")
-console.log(homeOne)
+const homeOne = document.querySelector(".home .increment-btn .one")
+const homeTwo = document.querySelector(".home .increment-btn .two")
+const homeThree = document.querySelector(".home .increment-btn .three")
+
+const guestOne = document.querySelector(".guest .increment-btn .one")
+const guestTwo = document.querySelector(".guest .increment-btn .two")
+const guestThree = document.querySelector(".guest .increment-btn .three")
 
 function increment(label) {
     score = Number(label.slice(-1))
     if (label.match("guest")) {
-        guestEl.innerHTML = guestScore;
+        guestScore += score;
         guestEl.innerHTML = guestScore;
     } else {
-        homeScore = homeScore + score;
+        homeScore += score;
         homeEl.innerHTML = homeScore;
     }
   }
 
-homeOne.addEventListener("click", function() {increment("home-1")}, false);
+homeOne.addEventListener("click", function() {increment("home-1")});
+homeTwo.addEventListener("click", function() {increment("home-2")});
+homeThree.addEventListener("click", function() {increment("home-3")});
+
+guestOne.addEventListener("click", function() {increment("guest-1")});
+guestTwo.addEventListener("click", function() {increment("guest-2")});
+guestThree.addEventListener("click", function() {increment("guest-3")});
